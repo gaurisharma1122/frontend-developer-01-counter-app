3aac60742cb8776b67636b697ca287e14739651a
gaurisharma1122 <gaurisharma1122@gmail.com>
2023-04-06 10:41:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [number, setNumber] = useState(0);
+  const [value, setValue] = useState(0);
+
+  const handleReset = () => {
+    setNumber(0);
+    setValue(0);
+  };
+  const handleIncrement = () => {
+    setNumber(number + parseInt(value));
+  };
+  const handleDecrement = () => {
+    setNumber(number - parseInt(value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className={ number<0 ? 'negative' : 'positive' }>{number}</h1>
+      <p>Enter a value by which increment/decrement should happen</p>
+      <input type="text" placeholder="Enter a number" value={value} onChange={(e) => setValue(e.target.value)} />
+      <div className="buttons">
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
 }
